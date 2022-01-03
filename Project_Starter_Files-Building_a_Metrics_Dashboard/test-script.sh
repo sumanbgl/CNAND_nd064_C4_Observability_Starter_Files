f2a3fd89305d6ee1d791eec60b217b8d7d612e3b
sumanbgl <suman.bgl@gmail.com>
2022-01-03 12:22:16 +0530

    Create test-script.sh

---
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/test-script.sh
@@ -0,0 +1,16 @@
+for in in 0 1 2 3 4 5 6 7 8 9
+do
+  curl http://localhost:8081/
+  curl http://localhost:8081/api
+  curl POST http://localhost:8081/star
+  curl http://localhost:8081/403
+  curl http://localhost:8081/404
+  curl http://localhost:8081/500
+  curl http://localhost:8081/503
+  
+  curl http://localhost:8080/  
+  curl http://localhost:8080/403
+  curl http://localhost:8080/404
+  curl http://localhost:8080/500
+  curl http://localhost:8080/503
+done
